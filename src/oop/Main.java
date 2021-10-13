b477613ccb48897101609c0365c3d933b429e355
Dermot <dermot.logue@dkit.ie>
2021-10-13 20:40:36 +0100

    Initial commit

---
+++ src/oop/Main.java
@@ -0,0 +1,58 @@
+package oop;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Random;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        ArrayList<String> studentList = new ArrayList<>();
+
+        studentList.add("James");
+        studentList.add("Adam");
+        studentList.add("Fred");
+
+        //TODO
+        // 1. add your own (two) first names to the ArrayList
+        // 2. write a method printList that will output the elements of the list
+        // 3. sort the ArrayList and then print out the sorted list
+        // 4. find the index position of "Fred", and assign it to a variable called 'index'
+        // 5. print out the value in the variable 'index'
+
+        // answer here
+
+
+        //TODO
+        // 6. Write a method randomName() that accepts the ArrayList and returns a name at random
+        // 7. Store the returned name in a variable and then output that variable's value
+        // 8. Prove that your method works - discuss !
+
+        // answer here
+
+
+        //TODO
+        // 9. Write a method filterByMaxLength() that accepts the studentList and a length (e.g.4)
+        // as arguments, and returns a new list of names that are less than or equal to the
+        // length passed in. Assign the returned list to a variable called shortNamesList.
+        // Print the shortNamesList.
+
+        // answer here
+
+
+    }
+
+//    public static void print( ? ) {
+//
+//    }
+//
+//    public static  ? randomName( ? ) {
+//
+//
+//    }
+//
+//    public static ? filterByMaxLength( ? ){
+//
+//    }
+}
